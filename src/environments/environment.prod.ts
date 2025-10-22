18afd7e5bce36bc2104d0d41d09eb723d7fb11a7
mghebro <159017172+mghebro@users.noreply.github.com>
2025-10-22 15:56:48 +0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: 'https://d0de21939d12.ngrok-free.app',
+  production: true,
+  apiUrl: 'https://be5045adb35e.ngrok-free.app',
   appleClientId: 'com.appleAuth.ai.service',
   appleRedirectUri: 'https://apple-test-ten.vercel.app/auth/apple/callback',
 };
