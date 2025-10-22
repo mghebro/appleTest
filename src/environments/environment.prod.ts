f1d1381f394194b040a8e0f1046c8a4bf86ea622
mghebro <giorgimgebrishvili2008@gmail.com>
2025-10-22 17:12:04 +0400

    new ngrok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://d27d80ccd4c0.ngrok-free.app',
+  apiUrl: 'https://55f563928a16.ngrok-free.app',
   appleClientId: 'com.appleAuth.ai.service',
   appleRedirectUri: 'https://apple-test-ten.vercel.app/auth/apple/callback',
 };
